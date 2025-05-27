f1888ed1506b0349d15a7224cd2fc37307159507
Sebastian Greg <fortunatos@roverkids.org>
2025-05-27 12:13:38 -0400

    unit 3.2

---
+++ JumpForce/Unit3/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float speed = 30;
+    private PlayerController playerControllerScript;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+    }
+}
